f561351e4143e534d8579c3bec2042ab4fabcc83
Thiyagu Loganathan <thiyaguelmails@gmail.com>
2016-12-29 16:40:01 +0530

    Update build.py

--- build.py
+++ build.py
@@ -1,5 +1,14 @@
-from pybuilder.core import use_plugin
+from pybuilder.core import use_plugin, init
 
 use_plugin("python.core")
+use_plugin("python.unittest")
+use_plugin("python.install_dependencies")
 
+
+name = "flask-peewee-example"
 default_task = "publish"
+
+
+@init
+def set_properties(project):
+    project.set_property("dir_source_main_python", ".")
