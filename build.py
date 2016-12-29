7345ecc82ff095a53709333c79e495151c763ae4
Thiyagu Loganathan <thiyaguelmails@gmail.com>
2016-12-29 17:14:25 +0530

    Update build.py

--- build.py
+++ build.py
@@ -2,9 +2,12 @@ from pybuilder.core import use_plugin, init
 
 use_plugin("python.core")
 
+description = "Demo Project for Blitz17"
 name = "flask-peewee-example"
-default_task = "publish"
+version = '0.0.1'
+default_task = ['clean', 'publish']
 
 @init
 def set_properties(project):
     project.set_property("dir_source_main_python", "example")
+    
